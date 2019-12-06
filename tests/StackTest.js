af0015dabd02ad23056b345746fd20030113d0c9
swarup das <swarup260@gmail.com>
2019-12-06 23:50:32 +0530

    describe updated

--- tests/StackTest.js
+++ tests/StackTest.js
@@ -4,21 +4,20 @@ const {
 } = require('../data_structure/Stack')
 
 describe('Stack', function () {
-    describe('Stack Object', function () {
-        const stack = new StackObject();
-        it('Must Be Instance of Stack Object', function () {
-            assert.equal(stack instanceof StackObject, true);
-        });
-        it('Push An Element to Stack And Length must be One', function () {
-            stack.push("Swarup")
-            assert.equal(stack.size(), 1);
-        });
-        it('Peek Element Must "Swarup" As its the top element of the stack', function () {
-            assert.equal(stack.peek(), "Swarup");
-        });
-        it('After Clear the Stack Must be Empty', function () {
-            stack.clear();
-            assert.equal(stack.size(), 0);
-        });
+    const stack = new StackObject();
+    it('Must Be Instance of Stack Object', function () {
+        assert.equal(stack instanceof StackObject, true);
     });
+    it('Push An Element to Stack And Length must be One', function () {
+        stack.push("Swarup")
+        assert.equal(stack.size(), 1);
+    });
+    it('Peek Element Must "Swarup" As its the top element of the stack', function () {
+        assert.equal(stack.peek(), "Swarup");
+    });
+    it('After Clear the Stack Must be Empty', function () {
+        stack.clear();
+        assert.equal(stack.size(), 0);
+    });
+    
 });
