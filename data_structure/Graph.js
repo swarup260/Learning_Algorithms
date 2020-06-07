f33bc841da22b4207a256a116ae6b7732604d8b3
swarup das <swarup260@gmail.com>
2020-06-07 20:18:48 +0530

    Graph DS added

--- data_structure/Graph.js
+++ data_structure/Graph.js
@@ -2,15 +2,6 @@ const {
     Dictionary
 } = require('./Dictionary');
 
-const { Queue } = require('./Queue')
-
-const Colors = {
-    WHITE : 0, //Vertex not visted 
-    GREY : 1, //Vertex  visted but not explored
-    BLACK : 2 //Vertex  explored Complete
-}
-
-
 class Graph {
     constructor(isDirected = false) {
         this.isDirected = isDirected;
@@ -58,61 +49,6 @@ class Graph {
 
 }
 
-
-
-const graph = new Graph();
-const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; //	{12}
-for (let i = 0; i < myVertices.length; i++) {
-    graph.addVertex(myVertices[i]);
+module.exports = {
+    Graph
 }
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('A', 'D');
-graph.addEdge('C', 'D');
-graph.addEdge('C', 'G');
-graph.addEdge('D', 'G');
-graph.addEdge('D', 'H');
-graph.addEdge('B', 'E');
-graph.addEdge('B', 'F');
-graph.addEdge('E', 'I');
-
-
-const colorInitialize = vertices => {
-    const colors = {};
-    vertices.forEach(vertex => {
-        colors[vertex] = Colors.WHITE;
-    });
-
-    return colors;
-}
-
-
-const BFS = (graph , initialVertice , callback) => {
-    const vertices = graph.getVertices();
-    const adjList = graph.getEdges();
-    const color = colorInitialize(vertices);    
-    const queue = new Queue();
-    queue.equeue(initialVertice);
-    while (!queue.isEmpty()){
-        const vertex = queue.dequeue();
-        color[vertex] = Colors.GREY;
-        let neighbores = adjList.get(vertex);
-        neighbores.forEach(node => {
-            if (color[node] === Colors.WHITE) {
-                queue.equeue(node);
-                color[node] = Colors.GREY;
-            }
-        });
-        color[vertex] = Colors.BLACK;
-        if (callback) {
-            callback(vertex);
-        }
-    }
-}
-
-BFS(graph,myVertices[0], (x) => console.log("Vertex Visited : ",x));
-
-
-
-// console.log(graph.getVertices());
-// console.log(graph.toString());
