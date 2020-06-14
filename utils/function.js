6f82ae45ad1f6c4d2b61a99bfa755a5f824f32e5
swarup das <swarup260@gmail.com>
2020-06-15 01:34:40 +0530

    swap function added

--- utils/function.js
+++ utils/function.js
@@ -16,7 +16,12 @@ const Compare = {
     LESS_THAN: -1,
     BIGGER_THAN: 1
 };
-
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns {Compare}
+ */
 function defaultCompare(a, b) {
     if (a === b) { // {1}
         return 0;
@@ -24,12 +29,20 @@ function defaultCompare(a, b) {
     return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN; // {2}
 }
 
-
-
+/**
+ * 
+ * @param {Array} array 
+ * @param {Number} index 
+ * @param {Number} nextIndex 
+ */
+const swap = (array , index , nextIndex) => {
+    [array[index] , array[nextIndex] ] = [ array[nextIndex] , array[index]];
+}
 
 
 module.exports = {
     toStringFunc,
     defaultCompare,
-    Compare
+    Compare,
+    swap
 }
