e698d62135303a2af5a11fa81e9df359f92998ad
swarup <swarup260@gmail.com>
2020-06-14 17:19:43 +0530

    Insertion sort algorithm added

---
+++ Algorithms/Sorting/InsertionSort.js
@@ -0,0 +1,29 @@
+const { Compare, defaultCompare } = require('../../utils/function');
+
+/**
+ * Sort the Element in a given array
+ * complexity of Insertion Sort is O(n^2)
+ * @param {array} array 
+ * @param {Function} compareFunc 
+ * @returns array
+ */
+const InsertionSort = (array, compareFunc = defaultCompare) => {
+    const { length } = array;
+    let i, j, temp;
+    for (let i = 1; i < length; i++) {
+        j = i;
+        temp = array[j];
+        while (j > 0 && compareFunc(array[j - 1], temp) == Compare.BIGGER_THAN) {
+            array[j] = array[j - 1];
+            j--;
+        }
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+// console.log(InsertionSort([5, 3, 1, 0, -1]));
+module.exports = {
+    InsertionSort
+}
