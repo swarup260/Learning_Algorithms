becfa7a8971a1413d961b79b54cea21f5c6a6c52
swarup das <swarup260@gmail.com>
2020-06-16 23:35:25 +0530

    new sorting algorithms added

---
+++ Algorithms/Sorting/CountingSort.js
@@ -0,0 +1,101 @@
+/**
+ * Sort the Element in a given array
+ * complexity of Counting Sort is O(n+k)
+ * Distributed and Interger Sorting Alogrithms
+ * @param {Array} array 
+ */
+const CountingSort = array => {
+    if (array.length < 2) {
+        return array;
+    }
+
+    const maxValue = findMaxValue(array);
+
+    const counts = new Array(maxValue + 1);
+    array.forEach(element => {
+        if (!counts[element]) {
+            counts[element] = 0;
+        }
+        counts[element]++;
+    });
+    let sortIndexed = 0;
+    counts.forEach((count, i) => {
+        while (count > 0) {
+            array[sortIndexed++] = i;
+            count--;
+        }
+    });
+    return array;
+}
+
+/**
+ * return the max. value in a given array
+ * @param {Array} array 
+ * @returns Number
+ */
+const findMaxValue = array => {
+    let max = array[0];
+    for (let index = 1; index < array.length; index++) {
+        if (array[index] > max) {
+            max = array[index];
+        }
+    }
+
+    return max;
+}
+
+
+/**
+ * Sort the Element in a given array
+ * complexity of Counting Sort is O(n+k)
+ * Distributed and Interger Sorting Alogrithms
+ * @param {Array} array 
+ */
+const CountingSortForNegative = array => {
+    if (array.length < 2) {
+        return array;
+    }
+    const {
+        min,
+        max
+    } = findMinMax(array);
+    const counts = {};
+    array.forEach(element => {
+        if (!counts[element]) {
+            counts[element] = 0;
+        }
+        counts[element]++;
+    });
+    for (let index = min, sortIndexed = 0; index <= max; index++) {
+        while (counts[index] > 0) {
+            array[sortIndexed++] = index;
+            counts[index]--;
+        }
+
+    }
+    return array;
+}
+
+
+const findMinMax = array => {
+    let min = Number.MAX_SAFE_INTEGER,
+        max = array[0];
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] < min) {
+            min = array[index];
+        }
+        if (array[index] > max) {
+            max = array[index];
+        }
+    }
+
+    return {
+        min,
+        max
+    }
+}
+
+console.time('CountingSort');
+console.log(CountingSortForNegative([5, 3, 3, 10, 1, 2, 0, -1, -3, 7, 7, 7, 7, 7, 7, 7]));
+// console.log(CountingSortForNegative([5, 4, 3, 2, 3, 1, 0]));
+console.timeEnd('CountingSort');
---
+++ Algorithms/Sorting/QuickSort.js
@@ -0,0 +1,60 @@
+const {
+    Compare,
+    defaultCompare,
+    swap
+} = require('../../utils/function');
+
+/**
+ * Sort the Element in a given array
+ * complexity of Quick Sort is O(n*long n)
+ * @param {array} array 
+ * @param {Function} compareFunc 
+ * @returns array
+ */
+const QuickSort = (array, compareFunc = defaultCompare) => {
+    console.log(array.length);
+    return quick(array, 0, array.length - 1, compareFunc);
+}
+
+
+const quick = (array, left, right, compareFunc) => {
+    let index;
+    if (array.length > 1) {
+        index = partition(array, left, right, compareFunc);
+        if (left < index - 1) {
+            quick(array, left, index - 1, compareFunc);
+        }
+        if (index < right) {
+            quick(array, index, right, compareFunc);
+        }
+    }
+    return array;
+}
+
+const partition = (array, left, right, compareFunc) => {
+    const pivot = array[Math.floor((left + right) / 2)];
+    console.log(pivot)
+    let i = left , j =right ;
+    while (i <= j) {
+        while (compareFunc(array[i], pivot) == Compare.LESS_THAN) {
+            i++;
+        }
+        while (compareFunc(array[j], pivot) == Compare.BIGGER_THAN) {
+            j--;
+        }
+        if (i <= j) {
+            swap(array, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+
+// console.time('QuickSort');
+// console.log(QuickSort([5, 3, 10, 1, 2, 0, -1, 7]));
+// console.timeEnd('QuickSort');
+module.exports = {
+    QuickSort
+}
